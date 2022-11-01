31ce7966d41fb9eb9dc4fd8519d08747e9bbfaff
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2022-11-01 10:34:01 +0300

    first commit

---
+++ new-task.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+CURRENT_TASK=$(aws ecs describe-task-definition --task-definition "${TASK_FAMILY}" --region "${AWS_DEFAULT_REGION}")
+
+NEW_TASK=$(echo $CURRENT_TASK | jq --arg IMAGE "${ECR_IMAGE}" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt) | del(.registeredBy)')
+
+NEW_TASK_INFO=$(aws ecs register-task-definition --region "${AWS_DEFAULT_REGION}" --cli-input-json "$NEW_TASK")
+
+NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')
+
+aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition "${TASK_FAMILY}:$NEW_REVISION"
